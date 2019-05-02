fa81f0cd2abd410432aece30985f8828bbdd963f
John Tsai <darkfeathers_0916@hotmail.com>
2019-05-02 15:10:32 +0800

    route 新增

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :statics , :only => [:index]
+  root "statics#index"
+
+  resources :rockets , :only =>[:index , :show]
+
+  namespace :dashboard do
+
+    resources :rockets
+
+    namespace :qnklkfdiosidfho do
+      resources :rockets
+      resources :trackinglists
+      resources :users
+      resources :managers
+
+    end
+  end
 end
