d0310b090bf545d2d806f8c3245fbf7daa5acdec
John Tsai <darkfeathers_0916@hotmail.com>
2019-05-15 16:41:23 +0800

    修正 controller

--- app/controllers/rockets_controller.rb
+++ app/controllers/rockets_controller.rb
@@ -1,9 +1,7 @@
 class RocketsController < ApplicationController
   def index
-    @rocket = @paginate = rocket.paginate(:page =>params[:page])
+    @rocket = @paginate = Rocket.paginate(:page =>params[:page])
   end
-
-  
   def show
     @rocket = Rocket.find(paramas[:id])
   end
