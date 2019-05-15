7c077d17bdaa1519e323f9ec111a9f11d56cf23f
darkfeatherstsai <45931302+darkfeatherstsai@users.noreply.github.com>
2019-05-15 16:48:44 +0800

    Revert "test"
    This reverts commit ab1d2b7edc28ad5cd0c4c237d68cfe7c5720675a.

--- app/controllers/rockets_controller.rb
+++ app/controllers/rockets_controller.rb
@@ -2,8 +2,6 @@ class RocketsController < ApplicationController
   def index
     @rocket = @paginate = rocket.paginate(:page =>params[:page])
   end
-
-  
   def show
     @rocket = Rocket.find(paramas[:id])
   end
