5400ad6cb8d870dabf323e094cc6e59b961dc7b4
John Tsai <darkfeathers_0916@hotmail.com>
2019-05-15 15:24:11 +0800

    test

--- app/controllers/rockets_controller.rb
+++ app/controllers/rockets_controller.rb
@@ -2,6 +2,8 @@ class RocketsController < ApplicationController
   def index
     @rocket = @paginate = rocket.paginate(:page =>params[:page])
   end
+
+  
   def show
     @rocket = Rocket.find(paramas[:id])
   end
