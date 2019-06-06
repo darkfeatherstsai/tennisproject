a81db8e28dc16f8a16819b7661695d38d0029533
John Tsai <darkfeathers_0916@hotmail.com>
2019-06-06 15:40:40 +0800

    新增追蹤功能

--- app/controllers/dashboard/trackinglists_controller.rb
+++ app/controllers/dashboard/trackinglists_controller.rb
@@ -2,4 +2,10 @@ class Dashboard::TrackinglistsController < Dashboard::DashboardController
   def index
     @rackets = @paginate = Racket.paginate(:page => params[:page])
   end
+  def add
+    trackinglist = Trackinglist.find{|list| list.user_id == current_user.id}
+    trackinglist = Trackinglist.create(:user_id => current_user.id) if trackinglist == nil
+    trackinglist.racket_id << params[:id].to_i if trackinglist.racket_id.include?(params[:id].to_i) == false
+    trackinglist.save
+  end
 end
