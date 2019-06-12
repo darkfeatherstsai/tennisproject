f10c289a555f5a934a20f612fa63be64be35c267
John Tsai <darkfeathers_0916@hotmail.com>
2019-06-12 22:04:06 +0800

    修改資料處理程式

--- lib/gocha_racket_url.rb
+++ lib/gocha_racket_url.rb
@@ -20,26 +20,29 @@ racket_urls.each do |racket_url|
 
   content = doc.search('meta').to_a[12].attr('content').split("\n")
   content.delete("")
-  if content[0].include?("買") == false
+
+  puts racket_url
+
+  if content[0].match?(/[買]/) == false && content[1].match?(/[鞋車日本袋]/) == false
     a = Racket.new
     a.name = content.select{|element| element.match(/[物品名稱]/)}[0].split(/[:：\}]/)[1]
     a.name.downcase!
-    if a.name.include?("wilson")
+    if a.name.include?("wil")
       a.label = "wilson"
-    elsif a.name.include?("babolat")
+    elsif a.name.include?("bab")
       a.label = "babolat"
-    elsif a.name.include?("yonex")
+    elsif a.name.include?("yon")
       a.label = "yonex"
-    elsif a.name.include?("head")
+    elsif a.name.include?("he")
       a.label = "head"
-    else a.name.include?("dunlop")
+    else a.name.include?("dun")
       a.label = "dunlop"
     end
 
     a.weight = content.select{|element| element.match(/[2-90]{3}/)}[0].match(/\d{3}/)[0]
-    a.price = content.select{|element| element.match(/\d{4}/)}[0].match(/\d{4}/)[0]
+    a.price = content.select{|element| element.match(/[售價元]/)}[0].match(/\d{4}/)[0]
     a.spec = content.select{|element| element.match(/[規格]/)}[0]
-    a.profile = content.select{|element| element.match(/[使用概況狀態]/)}[0].split(/[:：]/)[1]
+    a.profile = content.select{|element| element.match(/[使用概況狀態]/)}[0].split(/[:：]/)[1] if content.select{|element| element.match(/[使用概況狀態]/)}[0] != nil
     a.fb_url = racket_url
     a.save
   end
