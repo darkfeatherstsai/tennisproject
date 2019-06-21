3e9563278bcdba8868cde52c9b3ce9e2ca392850
John Tsai <darkfeathers_0916@hotmail.com>
2019-06-21 22:29:54 +0800

    修正追蹤以及取消追蹤功能，新增切換頁面

--- app/controllers/dashboard/trackinglists_controller.rb
+++ app/controllers/dashboard/trackinglists_controller.rb
@@ -1,21 +1,25 @@
 class Dashboard::TrackinglistsController < Dashboard::DashboardController
+  before_action :set_list
+
   def show
-    trackinglist = Trackinglist.find{|list| list.user_id == current_user.id}
-    @rackets = Racket.find(trackinglist.racket_id)
   end
 
   def add
-    trackinglist = Trackinglist.find{|list| list.user_id == current_user.id}
-    trackinglist = Trackinglist.create(:user_id => current_user.id) if trackinglist == nil
-    trackinglist.racket_id << params[:id].to_i if trackinglist.racket_id.include?(params[:id].to_i) == false
-    trackinglist.save
+    @trackinglist = Trackinglist.create(:user_id => current_user.id) if @trackinglist == nil
+    @trackinglist.racket_id << params[:id].to_i if @trackinglist.racket_id.include?(params[:id].to_i) == false
+    @trackinglist.save
   end
 
   def destroy
-    trackinglist = Trackinglist.find{|list| list.user_id == current_user.id}
-    trackinglist.racket_id.delete(params[:id].to_i)
-    trackinglist.save
-    @rackets = Racket.find(trackinglist.racket_id)
-    render :show
+    @trackinglist.racket_id.delete(params[:id].to_i)
+    @trackinglist.save
+    redirect_to :action => :index
+  end
+
+  private
+
+  def set_list
+    @trackinglist = Trackinglist.find{|list| list.user_id == current_user.id}
+    @rackets = Racket.find(@trackinglist.racket_id)
   end
 end
