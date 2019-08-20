1d5238fd584c194af6285c8a3dcf68dac1dc8a5d
John Tsai <darkfeathers_0916@hotmail.com>
2019-08-20 22:31:05 +0800

    新增 rake 任務 ＆ 搜尋程式修改

--- lib/gocha_racket_url.rb
+++ lib/gocha_racket_url.rb
@@ -9,7 +9,7 @@ require 'nokogiri'
 
     racket_urls = []
 
-    until racket_urls.size > 10
+    until racket_urls.size > 50
       driver.execute_script("window.scrollTo(0, document.documentElement.scrollHeight);")
       links = driver.find_elements(:class, "_5pcq")
       links.each do |link|
@@ -19,6 +19,7 @@ require 'nokogiri'
       sleep 5
     end
 
+    not_racket_url = []
     error_url = []
 
     racket_urls.each do |racket_url|
@@ -35,29 +36,26 @@ require 'nokogiri'
           content = unprocessed_content.split("\n")
         end
 
-        puts racket_url
-
-
         if content.first.match?("賣") && content.select{|element| element.match(/日本|[裝鞋車機衣包顆]|back/)}[0] == nil
           a = Racket.find_by(fb_url: racket_url)
           a ||= Racket.new
           a.name = content.select{|element| element.match(/[物品名稱]/)}[0].split(/[:：\}\s]/ , 2)[1].delete(":：")
           a.name.downcase!
-          if a.name.include?("wil")
+          if a.name.match?("wil")
             a.label = "wilson"
-          elsif a.name.include?("bab")
+          elsif a.name.match?("bab")
             a.label = "babolat"
-          elsif a.name.include?("yon")
+          elsif a.name.match?(/yon|yy/)
             a.label = "yonex"
-          elsif a.name.include?("he")
+          elsif a.name.match?("he")
             a.label = "head"
-          elsif a.name.include?("pri")
+          elsif a.name.match?("pri")
             a.label = "prince"
-          elsif a.name.include?("dun")
+          elsif a.name.match?("dun")
             a.label = "dunlop"
-          elsif a.name.include?("vol")
+          elsif a.name.match?("vol")
             a.label = "volkl"
-          elsif a.name.include?("tecn")
+          elsif a.name.match?("tecn")
             a.label = "tecnifibre"
           else
             a.label = "其他"
@@ -68,11 +66,12 @@ require 'nokogiri'
           end
 
           if content.select{|element| element.match(/售價|元|\$/)}[0] != nil
-            a.price = content.select{|element| element.match(/售價|元|\$/)}[0].match(/\d{4}/)[0]
+            match_ele = content.select{|element| element.match(/售價|元|\$/)}
+            a.price = match_ele.select{|element| element.match(/\d{4}/)}[0].match(/\d{4}/)[0]
           end
 
           if content.select{|element| element.match(/[規格]/)}[0] != nil
-            a.spec = content.select{|element| element.match(/[規格]/)}[0].delete("產品規格：:]")
+            a.spec = content.select{|element| element.match(/規格|拍面|握把|線床/)}.join.delete("產品規格]").slice(1..-1)
           end
 
           if content.select{|element| element.match(/使用|概況|狀態/)}[0] != nil
@@ -82,6 +81,9 @@ require 'nokogiri'
           a.fb_url = racket_url
           a.lunched = 1 if a.name.size < 50
           a.save
+
+        else
+          not_racket_url << racket_url
         end
 
       rescue
@@ -94,5 +96,8 @@ require 'nokogiri'
     error_url.each do |url|
       puts url
     end
+
+    puts error_url.count
+    puts not_racket_url.count
 
     driver.quit
