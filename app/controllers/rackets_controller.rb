cdb40f3b856d717ee29b7413d867a3512fb6407e
John Tsai <darkfeathers_0916@hotmail.com>
2019-09-04 16:48:12 +0800

    line-bot 關鍵字回應優化2

--- app/controllers/rackets_controller.rb
+++ app/controllers/rackets_controller.rb
@@ -52,20 +52,27 @@ class RacketsController < ApplicationController
   #關鍵字回覆
   def keyword_reply(received_text)
     found_racket = []
+
     begin
     if received_text[0..4] == "gocha"
       racket = received_text.split(",")
-      Racket.where(lunched: 1).where(weight: racket[2].to_i..racket[3].to_i).where(price: racket[4].to_i..racket[5].to_i).find_each do |r|
-        if r.name.include?(racket[1]) || r.label.include?(racket[1])
-          found_racket << "#{r.label} #{r.name} #{r.weight} #{r.price} #{r.fb_url}"
+      match_racket = Racket.where(lunched: 1).where(weight: racket[2].to_i..racket[3].to_i).where(price: racket[4].to_i..racket[5].to_i)
+      if match_racket.count > 0
+        match_racket.find_each do |r|
+          if r.name.include?(racket[1]) || r.label.include?(racket[1])
+            found_racket << "#{r.name} #{r.weight} #{r.price} #{r.fb_url}"
+          end
         end
+      else
+        return "沒有符合的球拍"
       end
+
       retuen found_racket.join("\n")
     end
 
-    #出現錯誤  
+    #出現錯誤
     rescue
-      return "沒有符合的球拍或是格式不符"
+      return "格式不符"
     end
 
   end
