66f2b1a6fbb8d6a920717dd9199c17d43ef1306b
John Tsai <darkfeathers_0916@hotmail.com>
2019-09-04 13:28:47 +0800

    Line-bot 調整

--- app/controllers/rackets_controller.rb
+++ app/controllers/rackets_controller.rb
@@ -33,7 +33,28 @@ class RacketsController < ApplicationController
   end
 
   def webhook
+    # 設定回覆文字
+    reply_text = keyword_reply(received_text)
 
+    # 傳送訊息到 line
+    response = reply_to_line(reply_text)
+
+    # 回應 200
+    head :ok
+  end
+
+  # 取得對方說的話
+  def received_text
+    message = params['events'][0]['message']
+    message['text'] unless message.nil?
+  end
+
+  #關鍵字回覆
+  def keyword_reply(received_text)
+    received_text
+  end
+
+  def reply_to_line(message)
     # 取得 reply token
     reply_token = params['events'][0]['replyToken']
 
@@ -43,11 +64,8 @@ class RacketsController < ApplicationController
       text: "#{Racket.last}"
     }
 
-    # 取得 reply token
-    response = client.reply_message(reply_token, message)
-
-    # 回應 200
-    head :ok
+    # 傳送訊息
+    line.reply_message(reply_token, message)
   end
 
   # Line Bot API 物件初始化
