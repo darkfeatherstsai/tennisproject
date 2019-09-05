fbbbd0b45ac83d9a40a36fbf333aa34773302d3a
John Tsai <darkfeathers_0916@hotmail.com>
2019-09-05 14:09:17 +0800

    嘗試讓 line-bot 發送放不下的訊息

--- app/controllers/rackets_controller.rb
+++ app/controllers/rackets_controller.rb
@@ -73,20 +73,27 @@ class RacketsController < ApplicationController
     else
       return "沒有符合的球拍"
     end
-    if found_racket.count > 3
-      5.times do
-        response = push_to_line("U93bfce2b250065c001f5bdb965419083")
+
+    if found_racket.count > 5
+      index = 0
+      until index > found_racket.count
+        response = push_to_line(found_racket[index..(index+4)])
+        index += 5
       end
     end
     return found_racket.join("\n")
   end
 
-  def push_to_line(userid)
-
+  def push_to_line(racket)
+    # "U93bfce2b250065c001f5bdb965419083"
+    userid = params['events'][0]['userId']
+    p "==============="
+    p userid
+    p "==============="
     # 設定回覆訊息
     message = {
       type: 'text',
-      text: "發話測試"
+      text: racket
     }
 
     # 傳送訊息到 line
